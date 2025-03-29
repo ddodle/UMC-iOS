e3992ae412b86be7cdf5c25548fdc3a78a23c0b1
Doyeon Kim <ehdus022191@dgu.ac.kr>
2025-03-29 19:40:05 +0900

    Feat : AdView

---
+++ Starbucks/Views/AdView.swift
@@ -0,0 +1,56 @@
+//
+//  AdView.swift
+//  Starbucks
+//
+//  Created by 김도연 on 3/29/25.
+//
+
+import SwiftUI
+
+struct AdView: View{
+    @Environment(\.dismiss) private var dismiss
+    
+    var body: some View{
+        VStack{
+            Image("ad")
+                .resizable()
+                .frame(maxWidth: .infinity)
+            
+            Spacer()
+            
+            buttons
+        }
+    }
+    
+    var buttons: some View{
+        VStack(spacing: 12){
+            Button(action: {
+                print("자세히보기")
+            }, label: {
+              Text("자세히보기")
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+            })
+            .frame(height: 48)
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.capsule)
+            .tint(.green00)
+            
+            HStack{
+                Spacer()
+
+                Button(action: {
+                    dismiss()
+                }, label: {
+                    Text("X 닫기")
+                        .foregroundColor(.gray)
+                        .font(.body)
+                })
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    AdView()
+}
